11df2e06d174c7183c52af0b060c4b5d5c199c54
jessicashong <jessicashong12@gmail.com>
2023-03-08 12:37:11 -0600

    added files and created react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Section from './components/Section'
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Header />
+      <Section />
+    </div>
+  );
+}
+
+export default App;
