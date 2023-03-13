5d7af3b4e27f23d6db408b38126fa685ee58cb70
jessicashong <jessicashong12@gmail.com>
2023-03-13 18:36:21 -0500

    working on contact page form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+// import { checkPassword, validateEmail } from "";
 
-export default function Contact() {
+function ContactForm() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleInputChange = (e) => {
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     return (
         <div class="form-floating mb-3">
+            <input type="name" class="form-control" id="floatingInput" placeholder="Name: ">
+            <label for="floatingInput">Name: </label><br/>
+
             <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput">Email address</label>
+            <label for="floatingInput">Email: </label>
+
+            <input type="message" class="form-control" id="floatingInput" placeholder="Message: ">
+            <label for="floatingInput">Message: </label>
+
+            <button class="btn btn-primary" type="submit">Submit</button>
         </div>
-<div class="form-floating">
-  <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-  <label for="floatingPassword">Password</label>
-</div>
+
     )
 }
