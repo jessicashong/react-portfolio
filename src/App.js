6a51cc6059f3e8224e4af5ffa2ee1a18e49bc2a6
jessicashong <jessicashong12@gmail.com>
2023-03-14 18:46:43 -0500

    figuring out styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PortfolioContainer from './components/PortfolioContainer';
-import './App.css'
+import './styles/App.css'
 // import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 
