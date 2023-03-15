4d2d373ddc0ef4e92f0bd2e83926396db906a83f
jessicashong <jessicashong12@gmail.com>
2023-03-14 20:46:59 -0500

    testing deployment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import PortfolioContainer from './components/PortfolioContainer';
-import './styles/App.css'
-// import NavBar from './components/NavBar';
 import Footer from './components/Footer';
+import './styles/App.css';
 
 function App() {
   return (
     <div>
-      {/* <NavBar /> */}
       <PortfolioContainer />
       <Footer />
     </div>
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,15 +1,17 @@
 import React from "react";
+import avatar from '../../images/avatar_hi.jpg';
 
 function About() {
   return (
-    <div>
-      <h1>Hey guys! I'm a header!</h1>
-      <h2>Things I like!</h2>
-      <ul>
-        <li>Peas</li>
-        <li>Carrots</li>
-        <li>Love</li>
-      </ul>
+    <div className="row">
+      <h1>Jessica Hong</h1>
+      <div className="col">
+        <img src={avatar} alt="jess avatar" />
+      </div>
+      <div className="col">
+        <h2>A little bit about me: </h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
     </div>
   );
 }
